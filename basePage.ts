285cbc5c03dcca03b5a35dd21690529cb716a2e2
KendraJones <kendralee.jones81@gmail.com>
2022-08-29 19:34:47 -0600

    first commit

---
+++ basePage.ts
@@ -0,0 +1,45 @@
+import { Builder, By, WebDriver, WebElement, Capabilities, until } from "selenium-webdriver";
+import { Driver } from "selenium-webdriver/chrome";
+const chromedriver = require("chromedriver");
+
+interface Options {
+    driver?: Driver
+    url?: string
+};
+
+export class BasePage {
+    driver: WebDriver
+    url: string
+
+    constructor(options?: Options) {
+        if(options && options.driver) this.driver = options.driver
+        else
+        this.driver = new Builder().withCapabilities(Capabilities.chrome()).build()
+        if(options && options.url) this.url = options.url;
+    };
+    async navigate(url?: string): Promise<void> {
+        if (url) return await this.driver.get(url)
+        else if (this.url) return await this.driver.get(this.url)
+        else return Promise.reject('There is no URL');
+    };
+    async getElement(elementBy: By): Promise<WebElement> {
+        await this.driver.wait(until.elementLocated(elementBy))
+        let element = await this.driver.findElement(elementBy)
+        await this.driver.wait(until.elementIsVisible(element))
+        return element;
+    };
+    async click(elementBy: By): Promise<void> {
+        return(await this.getElement(elementBy)).click();
+    };
+    async setInput(elementBy: By, keys: any): Promise<void> {
+        let input = await this.getElement(elementBy)
+        await input.clear()
+        return input.sendKeys(keys);
+    };
+    async getText(elementBy: By): Promise<string> {
+        return (await this.getElement(elementBy)).getText();
+    };
+    async getAttribute(elementBy: By, attribute: string) {
+        return (await this.getElement(elementBy)).getAttribute(attribute);
+    };
+}
