304990b20576788b060b4d15feff513148560ca7
KendraJones <kendralee.jones81@gmail.com>
2022-08-31 18:31:34 -0600

    commiting 8-31-22

--- kendraBasePage.ts
+++ kendraBasePage.ts
@@ -5,7 +5,8 @@ export class kendraBasePage extends BasePage {
     search: By = By.name('search');
     results: By = By.id('firstHeading');
     english: By = By.id('js-link-box-en')
-    currentEvents: By = By.xpath('//*[@id="mw-content-text"]/div[1]/div/div[3]/ul/li[1]/a');
+    currentEvents: By = By.xpath('//*[@id="n-currentevents"]/a/span');
+    cEvents: By = By.id('Topics_in_the_news')
     mainPage: By = By.id('n-mainpage-description');
     mainText: By = By.id('Welcome_to_Wikipedia')
     mycoText: By = By.xpath('//*[@id="mw-content-text"]/div[3]/ul/li[1]/div[1]/a/span')
--- kendraTest.test.ts
+++ kendraTest.test.ts
@@ -13,34 +13,14 @@ test('Testing functionality of the Search Bar', async () => {
         else console.log("Screenshot Successful!")
     });
 });
-test('Testing the side bar links, Current Events and Main Page', async () => {
+test('Testing the functionality of the Side Bar', async () => {
     await kendra.navigate()
     await kendra.click(kendra.english)
     await kendra.click(kendra.currentEvents)
-    let text = await kendra.getResults()
-    expect(text).toContain('Worldwide current events');
-    await kendra.click(kendra.mainPage)
-    let mainText = await kendra.getResults()
-    expect(mainText).toContain('Welcome to ')
-    await fs.writeFile(`${__dirname}/screenshots/mainPage.png`, await kendra.driver.takeScreenshot(), 'base64', (e) => {
-        if (e) console.error(e)
-        else console.log("Screenshot Successful!")
-    });
-});
-test('Testing editing an article', async () => {
-    await kendra.navigate()
-    await kendra.click(kendra.search)
-    await kendra.searchBar('mycology')
-    let mycoText = await kendra.getResults()
-    expect(mycoText).toContain('Mycology')
-    await kendra.click(kendra.edit)
-    await kendra.getElement(kendra.startEditing) // Unable to interact with this, but it is needed in order to Edit
-    await kendra.click(kendra.textArea)
-    await kendra.setInput(kendra.textArea, 'Mycology is cool!')
-    await kendra.click(kendra.showPreview)
-    let preview = await kendra.getResults()
-    expect(preview).toContain('mycology is cool!')
-    await fs.writeFile(`${__dirname}/screenshots/editPage.png`, await kendra.driver.takeScreenshot(), 'base64', (e) => {
+    await kendra.getElement(kendra.cEvents)
+    let ceText = await kendra.getResults()
+    expect(ceText).toContain('Current events')
+    await fs.writeFile(`${__dirname}/screenshots/ce.png`, await kendra.driver.takeScreenshot(), 'base64', (e) => {
         if (e) console.error(e)
         else console.log("Screenshot Successful!")
     });
